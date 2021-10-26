d948542f766b3fcb11cc46c5a38990c97db49951
Santiago Bueno <nexo30bb@hotmail.com>
2021-10-26 14:28:47 -0300

    Cambio en el footer y header

--- views/footerT.php
+++ views/footerT.php
@@ -8,8 +8,8 @@
  <aside class="logo"><img src=public/imagenes/articulos/Logo2.png width="280px"></aside>
 <aside class="informacion"><ul class="contact-details">
 <li><i class="fas fa-map-marker-alt"></i> Dirección: Avenida Imagina, Las Piedras</li>
-<li><i class="fab fa-phonr"></i> Call Center: <a href="tel:+59824002451">2369 5734</a></li>
-<li><i class="far fa-envelope"></i> Email: <a href="mailto:ventas@chaparei.com">ventas@TecDev.com</a></li>
+<li><i class="fab fa-phonr"></i> Call Center: <a href="tel:+093823455">2369 5734</a></li>
+<li><i class="far fa-envelope"></i> Email: <a href="mailto:TecDev-TC@hotmail.com">TecDev-TC@hotmail.com</a></li>
 <li><i class="far fa-clock"></i> Horarios: Lunes a viernes: 08:30 a 13:00 – 13:30 – 18:00</li>
 </ul></aside>
 
