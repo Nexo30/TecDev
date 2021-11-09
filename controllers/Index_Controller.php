8bf06b7452391738361a56a3e09d910122c80006
Nicolas Gomez <nexo30bb@hotmail.com>
2021-11-09 20:28:37 -0300

    Agregado ingresar en index_controller

--- controllers/Index_Controller.php
+++ controllers/Index_Controller.php
@@ -12,11 +12,33 @@ class Index_Controller extends Controller
 
     public function render()
     {
+        //$alumnos = $this->model->get();
+        $this->view->alumnos = "cargado";
         $this->view->render('index/index');
     }
 
-    public function saludo()
+    public function ingresar()
     {
-        echo "<p>Hola a todos<p>";
+        //$alumnos = $this->model->get();
+        //$this->view->alumnos = "exitoso";
+        //$this->view->post = var_dump($_POST);
+        $nombre = $_POST['nombre'];
+        $pass = $_POST['pass'];
+        $exitoLogin = $this->model->ingresar($nombre, $pass);
+        if ($exitoLogin) {
+            $_SESSION["estalogueado"] = true;
+            $_SESSION["nombre"] = $nombre;
+            $this->view->render('login/ingresar');
+        } else {
+            $this->view->resultadoLogin = "usuario o contraseña incorrectos";
+            $this->view->render('login/index');
+        }
+
+    }
+    public function salir()
+    {
+        $_SESSION["estalogueado"] = false;
+        unset($_SESSION["nombre"]);
+        $this->view->render('index/index');
     }
 }
