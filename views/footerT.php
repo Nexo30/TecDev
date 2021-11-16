da49eeabaac5df65e9013aec8d94cad9cdd5a1ff
Santiago Bueno <nexo30bb@hotmail.com>
2021-11-15 22:38:23 -0300

    Agregado correcto gmail

--- views/footerT.php
+++ views/footerT.php
@@ -14,7 +14,7 @@
 <aside class="informacion"><ul class="contact-details">
 <li><i class="fas fa-map-marker-alt"></i> Dirección: Avenida Imaginaria, Las Piedras</li>
 <li><i class="fas fa-phone-alt"></i> Call Center: <a href="tel:+093823455">2369 5734</a></li>
-<li><i class="far fa-envelope"></i> Email: <a href="mailto:HTM-Consultas@hotmail.com">HTM-Consultas@hotmail.com</a></li>
+<li><i class="far fa-envelope"></i> Email: <a href="mailto:HTMConsultas@gmail.com">HTMConsultas@gmail.com</a></li>
 <li><i class="far fa-clock"></i> Horarios: Lunes a viernes: 08:30 a 13:00 – 13:30 – 18:00</li>
 </ul></aside>
 
