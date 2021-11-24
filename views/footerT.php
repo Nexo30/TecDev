e3503d814790a599b210f75f37bdafdd5a054a0b
Nicolas Gomez <nexo30bb@hotmail.com>
2021-11-24 00:10:57 -0300

    Arreglo detalle footer

--- views/footerT.php
+++ views/footerT.php
@@ -15,7 +15,7 @@
 <li><i class="fas fa-map-marker-alt"></i> Dirección: Avenida Imaginaria, Las Piedras</li>
 <li><i class="fas fa-phone-alt"></i> Call Center: <a href="tel:+093823455">2369 5734</a></li>
 <li><i class="far fa-envelope"></i> Email: <a href="mailto:HTMConsultas@gmail.com">HTMConsultas@gmail.com</a></li>
-<li><i class="far fa-clock"></i> Horarios: Lunes a viernes: 08:30 a 13:00 – 13:30 – 18:00</li>
+<li><i class="far fa-clock"></i> Horarios: Lunes a viernes: 08:30 a 13:00 – 13:30 a 18:00</li>
 </ul></aside>
 
 </footer>
